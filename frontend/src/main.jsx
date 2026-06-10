b0d079ab84e86563c589e5b7d0402924f3e55111
DG-houcine98 <lahoucine.elhaouri@datadoghq.com>
2026-06-10 09:48:31 +0200

    Frontend: install + init Datadog Browser RUM SDK

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { datadogRum } from '@datadog/browser-rum'
 import './index.css'
 import App from './App.jsx'
 import EditDog from './EditDog.jsx'
+
+datadogRum.init({
+  applicationId: 'd414b7c4-2108-4f2a-9f25-eff381c2bad5',
+  clientToken: 'pub18d20e529c36d6611797c708aedfd9b3',
+  site: 'datadoghq.com',
+  service: 'lahoucine-app-frontend',
+  env: 'prod',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  defaultPrivacyLevel: 'mask-user-input',
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
